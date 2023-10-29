cde108c93756535c48a089e4cc810a2e8026b27f
littleyang0531 <littleyang0531@outlook.com>
2023-10-29 14:25:29 +0800

    Update Ackee

--- api/index.js
+++ api/index.js
@@ -8,6 +8,74 @@ const https = require('https')
 const port = 3000;
 var app = new express();
 
+const send = function(url, body, opts) {
+    console.log(JSON.stringify(body));
+    var req = https.request({
+        hostname: "ackee.littleyang.me",
+        path: "/api",
+        method: "POST",
+        headers: {
+            Authorization: "Bearer 3e2eff9e-fe52-4462-a11b-f3cdeb2e1850",
+            "Content-Type": "application/json",
+            "Content-Length": JSON.stringify(body).length,
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76",
+            Accept: "*/*",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Origin": "https://ackee.littleyang.me",
+            "Referer": "https://ackee.littleyang.me/",
+            "Time-Zone": "Asia/Shanghai",
+        }
+    }, function(res) {
+        res.on('data', chunk => {
+            console.log(chunk.toString());
+        });
+    });
+    req.write(JSON.stringify(body));
+    req.on('error', (e) => {});
+    req.end();
+}
+
+const createRecordBody = function(domainId, input) {
+	return [{
+        operationName: "createRecord",
+		query: `
+			mutation createRecord($domainId: ID!, $input: CreateRecordInput!) {
+				createRecord(domainId: $domainId, input: $input) {
+					payload {
+						id
+					}
+				}
+			}
+		`,
+		variables: {
+			domainId,
+			input
+		}
+	}]
+}
+
+app.use(function (req, res, next) {
+    const api = "https://ackee.littleyang.me/api";
+    const id = "a59ca492-3bc4-44dc-b39e-9dc83751e0c3";
+    const value = (v) => {
+        return v === undefined || v === null ? "" : v;
+    };
+    pl = platform.parse(req.headers["user-agent"]);
+    const data = {
+		siteLocation: value("https://wds.server.littleyang.me" + req.path),
+		siteReferrer: value(req.headers.referer),
+		source: value(req.query.source),
+		deviceName: value(pl.product),
+		deviceManufacturer: value(pl.manufacturer),
+		osName: value(pl.os.family),
+		osVersion: value(pl.os.version),
+		browserName: value(pl.name),
+		browserVersion: value(pl.version)
+	};
+    send(api, createRecordBody(id, data), {});
+    next();
+});
+
 var config = JSON.parse(fs.readFileSync(path.join(__dirname, "../config.json")));
 
 function DFS(json, dep) {
@@ -149,74 +217,6 @@ sonolusApp.engineListHandler = (sonolus, query, page) => {
         infos: infos
     };
 }
-
-const send = function(url, body, opts) {
-    console.log(JSON.stringify(body));
-    var req = https.request({
-        hostname: "ackee.littleyang.me",
-        path: "/api",
-        method: "POST",
-        headers: {
-            Authorization: "Bearer 3e2eff9e-fe52-4462-a11b-f3cdeb2e1850",
-            "Content-Type": "application/json",
-            "Content-Length": JSON.stringify(body).length,
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76",
-            Accept: "*/*",
-            "Accept-Encoding": "gzip, deflate, br",
-            "Origin": "https://ackee.littleyang.me",
-            "Referer": "https://ackee.littleyang.me/",
-            "Time-Zone": "Asia/Shanghai",
-        }
-    }, function(res) {
-        res.on('data', chunk => {
-            console.log(chunk.toString());
-        });
-    });
-    req.write(JSON.stringify(body));
-    req.on('error', (e) => {});
-    req.end();
-}
-
-const createRecordBody = function(domainId, input) {
-	return [{
-        operationName: "createRecord",
-		query: `
-			mutation createRecord($domainId: ID!, $input: CreateRecordInput!) {
-				createRecord(domainId: $domainId, input: $input) {
-					payload {
-						id
-					}
-				}
-			}
-		`,
-		variables: {
-			domainId,
-			input
-		}
-	}]
-}
-
-app.use(function (req, res, next) {
-    const api = "https://ackee.littleyang.me/api";
-    const id = "a59ca492-3bc4-44dc-b39e-9dc83751e0c3";
-    const value = (v) => {
-        return v === undefined || v === null ? "" : v;
-    };
-    pl = platform.parse(req.headers["user-agent"]);
-    const data = {
-		siteLocation: value("https://wds.server.littleyang.me" + req.path),
-		siteReferrer: value(req.headers.referer),
-		source: value(req.query.source),
-		deviceName: value(pl.product),
-		deviceManufacturer: value(pl.manufacturer),
-		osName: value(pl.os.family),
-		osVersion: value(pl.os.version),
-		browserName: value(pl.name),
-		browserVersion: value(pl.version)
-	};
-    send(api, createRecordBody(id, data), {});
-    next();
-});
 
 app.use(express.static(path.join(__dirname, "../public/web")));
 
