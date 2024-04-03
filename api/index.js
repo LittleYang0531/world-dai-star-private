d00f77c4b7f84f8a7aaaf1729d88f9160892b019
LittleYang0531 <littleyang0531@outlook.com>
2024-04-03 14:58:50 +0000

    Update

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const sqlite3 = require("better-sqlite3");
-const db = new sqlite3("../sonolus.db", { readonly: true });
+console.log(__dirname + "/../public/sonolus.db")
+const db = new sqlite3(__dirname + "/../public/sonolus.db", { readonly: true });
 console.log(db.prepare("SELECT * FROM sqlite_master WHERE type='table'").all());
 const factory = require('../public/libsonolus.js');
 const app = express();
