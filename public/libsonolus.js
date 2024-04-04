b7288871be01176c34b5c3cb8e43f1e776337ba6
littleyang0531 <littleyang0531@outlook.com>
2024-04-04 11:14:05 +0800

    Update

--- public/libsonolus.js
+++ public/libsonolus.js
@@ -1481,6 +1481,7 @@ function dbg(text) {
     };
 
   var ___builtin_emscripten_execute = (sql) => {
+  		  return 0
           throw new Error("Cannot execute sql in node environment")
           let configJson = FS.readFile("/config/config.json", { encoding: "utf8" }).toString()
           let appConfig = JSON.parse(configJson)
